0744df79469901499f7b2ec6e2a5120555eea0f0
tushig tse <tsetushig@gmail.com>
2026-05-21 00:48:04 +0000

    Try fixing this error: `Runtime ReferenceError: ArrowRight is not define

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 import { motion } from "framer-motion"
-import { ExternalLink, Github, ArrowUpRight } from "lucide-react"
+import { ExternalLink, Github, ArrowUpRight, ArrowRight } from "lucide-react"
 import Image from "next/image"
 
 const projects = [
